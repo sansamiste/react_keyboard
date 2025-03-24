e68ec63765b9448520bfb335f82fb88b61b4c160
sansamiste <127095130+sansamiste@users.noreply.github.com>
2025-03-24 17:18:01 +0200

    fix solution

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,8 @@ export class App extends Component<{}, State> {
         <p className="App__message">
           {this.state.pressedKey
             ? `The last pressed key is [${this.state.pressedKey}]`
-            : 'Nothing was pressed yet'}
+            : // eslint-disable-next-line prettier/prettier
+            'Nothing was pressed yet'}
         </p>
       </div>
     );
